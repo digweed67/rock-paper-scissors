bbf31af13cf8b42e5643bfb80b3c17e328aee603
digweed67 <124364643+digweed67@users.noreply.github.com>
2024-02-21 11:20:30 +0100

    complete basic html and css and add playerSelection javaScript into it

---
+++ main.js
@@ -0,0 +1,45 @@
+
+
+/* function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+
+/* function displayComputerChoice () {
+    let computerSelection = getComputerChoice();
+    document.getElementById("computerChoiceDisplay").textContent = `Computer choice: ${computerSelection}`;
+} 
+*/ 
+
+
+
+let playerSelection; 
+
+function setPlayerChoice (choice) {
+    playerSelection = choice; 
+    displayPlayerChoice(); 
+}
+
+function displayPlayerChoice () {
+    document.getElementById("playerChoiceDisplay").textContent = `Your choice: ${playerSelection}`; 
+}
+
+
+
+
+let rockImage = document.querySelector(".rock").addEventListener("click", function () {
+    setPlayerChoice("rock");
+});
+ 
+
+let paperImage = document.querySelector(".paper").addEventListener("click", function() {
+    setPlayerChoice("paper");
+});
+
+let scissorsImage = document.querySelector(".scissors").addEventListener("click", function() {
+    setPlayerChoice("scissors");
+});
+
+
