3e09cb02876736d2ea6ccafb3e1c71bdccd91893
digweed67 <124364643+digweed67@users.noreply.github.com>
2024-02-28 13:43:39 +0100

    add playRound and eventListeners in the function

--- main.js
+++ main.js
@@ -31,32 +31,41 @@ function displayPlayerChoice () {
 let rockImage = document.querySelector(".rock").addEventListener("click", function () {
     setPlayerChoice("rock");
     displayComputerChoice ();
+    playRound(playerSelection, computerSelection);
 });
  
 
 let paperImage = document.querySelector(".paper").addEventListener("click", function() {
     setPlayerChoice("paper");
     displayComputerChoice ();
+    playRound(playerSelection, computerSelection);
 });
 
 let scissorsImage = document.querySelector(".scissors").addEventListener("click", function() {
     setPlayerChoice("scissors");
     displayComputerChoice ();
+    playRound(playerSelection, computerSelection);
 });
 
 
-/* round 
-function playRound(playerSelection, computerSelection) {
+let playerScore = 0;
+let computerScore = 0; 
+for (let i = 0; i < 5; i++) {
+
+        function playRound(playerSelection, computerSelection) {
+            
+            if (playerSelection === computerSelection){
+                document.getElementById("result").textContent = "It's a tie!";
+            } else if (
+                (playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "paper") ||
+                (playerSelection === "paper" && computerSelection === "rock") 
+                ) {
+                document.getElementById("result").textContent = `You win! ${playerSelection} beats ${computerSelection}`;
     
-    if (playerSelection === computerSelection){
-        document.getElementById("result").textContent = "It's a tie!";
-    } else if (
-        (playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "paper") ||
-        (playerSelection === "paper" && computerSelection === "rock") 
-        ) {
-        document.getElementById("result").textContent = (`You win! ${playerSelection} beats ${computerSelection}`);
-    } else {
-        document.getElementById("result").textContent = "Computer wins!"
+ 
+            } else {
+                document.getElementById("result").textContent = "Computer wins!"
+            }
+        }
     }
-}
-*/
+
